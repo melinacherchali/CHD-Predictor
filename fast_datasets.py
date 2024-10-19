e49b67a8621b732191fb90aa828fd0f4ab0320c5
Léo <leo.cusumano@epfl.ch>
2024-10-19 17:44:52 +0200

    fast file

--- fast_datasets.py
+++ fast_datasets.py
@@ -8,16 +8,18 @@ import helpers as hp
 
 
 
-abs_path="/Users/leocusumano/myCloud/EPFL/MA1/ML/Proj1/DATASETS/dataset"
-# x_train_, x_test_, y_train_, train_ids_, test_ids_ =hp.load_csv_data(abs_path,True)
+# abs_path="/Users/leocusumano/myCloud/EPFL/MA1/ML/Proj1/DATASETS/dataset"
+# x_train_, x_test_, y_train_, train_ids_, test_ids_ =hp.load_csv_data(abs_path)
 
-#save the data in a file to load it faster
+# # save the data in a file to load it faster
 
-# np.save("f_x_train_",x_train_)
-# np.save("f_x_test_",x_test_)
-# np.save("f_y_train_",y_train_)
-# np.save("f_train_ids_",train_ids_)
-# np.save("f_test_ids_",test_ids_)
+# path_to_save="/Users/leocusumano/myCloud/EPFL/MA1/ML/Proj1/DATASETS/f_sets/"
+
+# np.save(path_to_save+"f_x_train_",x_train_)
+# np.save(path_to_save+"f_x_test_",x_test_)
+# np.save(path_to_save+"f_y_train_",y_train_)
+# np.save(path_to_save+"f_train_ids_",train_ids_)
+# np.save(path_to_save+"f_test_ids_",test_ids_)
 
 
 
