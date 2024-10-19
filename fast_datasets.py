ffae36c7def63cc683600c049eae1ef2b5c50053
Léo <leo.cusumano@epfl.ch>
2024-10-19 17:32:52 +0200

    fast data loading files

---
+++ fast_datasets.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+import helpers as hp
+
+
+
+# data loading
+
+
+
+abs_path="/Users/leocusumano/myCloud/EPFL/MA1/ML/Proj1/DATASETS/dataset"
+# x_train_, x_test_, y_train_, train_ids_, test_ids_ =hp.load_csv_data(abs_path,True)
+
+#save the data in a file to load it faster
+
+# np.save("f_x_train_",x_train_)
+# np.save("f_x_test_",x_test_)
+# np.save("f_y_train_",y_train_)
+# np.save("f_train_ids_",train_ids_)
+# np.save("f_test_ids_",test_ids_)
+
+
+
