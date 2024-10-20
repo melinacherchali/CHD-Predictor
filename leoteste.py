91e04830c28651881e76ffc76c87988d1dc37f47
Léo <leo.cusumano@epfl.ch>
2024-10-20 18:14:53 +0200

    new drop nan func

--- leoteste.py
+++ leoteste.py
@@ -9,6 +9,7 @@ path="/Users/leocusumano/myCloud/EPFL/MA1/ML/Proj1/DATASETS/f_sets/"
 x_train=np.load(path+"f_x_train_.npy")
 x_test=np.load(path+"f_x_test_.npy")
 y_train=np.load(path+"f_y_train_.npy")
+test_ids=np.load(path+"f_test_ids_.npy")
 
 nan_col_ratio=(np.isnan(x_train).sum(axis=0)/x_train.shape[0])
 nan_row_ratio=(np.isnan(x_train).sum(axis=1)/x_train.shape[1])
@@ -28,25 +29,32 @@ plt.colorbar(label="SQRT(rowNaNratio*colNaNratio)")
 
 plt.show()
 
-def nan_to_drop(data,dual_nan_ratio_thr=0.7):
+def nan_to_drop(data,dual_nan_ratio_thr):
     i_drop,j_drop=np.where(data>dual_nan_ratio_thr)
     return np.unique(i_drop),np.unique(j_drop)
 
-def drop_col_row_nan(data):
+def drop_col_row_nan(data_x,data_y,nan_trd=0.7):
     nan_col_ratio=(np.isnan(x_train).sum(axis=0)/x_train.shape[0])
     nan_row_ratio=(np.isnan(x_train).sum(axis=1)/x_train.shape[1])
     x,y=np.meshgrid(nan_col_ratio,nan_row_ratio)
     # to find a score for each row and column that take into account the NaN ratio
     # we can multiply the two matrices and take the square root
     dual_ratio=np.sqrt(x*y)
-    i_drop,j_drop=nan_to_drop(dual_ratio)
-    w_row=np.delete(data,i_drop,axis=0)
-    w_col=np.delete(w_row,j_drop,axis=1)
-    return w_col
+    i_drop,j_drop=nan_to_drop(dual_ratio,nan_trd)
+    w_row_x=np.delete(data_x,i_drop,axis=0)
+    w_colrow_x=np.delete(w_row_x,j_drop,axis=1)
+    w_row_y=np.delete(data_y,i_drop,axis=0)
+    
+    return w_colrow_x,w_row_y
 
-dataclean=drop_col_row_nan(x_train)
+dataclean_x,dataclean_y=drop_col_row_nan(x_train,y_train)
 
-final,test=pp.clean_data(dataclean,x_test,1,0.1,0.1)
+final,test=pp.clean_data(dataclean_x,x_test,1,0.1,0.1)
+
+
+
+
+
 
 
 
