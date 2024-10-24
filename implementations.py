a6f219ae5b18b54ab01246fdbd4fc2c19693b64e
Melina <melina.cherchali@epfl.ch>
2024-10-24 23:44:05 +0200

    clean up

--- implementations.py
+++ implementations.py
@@ -1,10 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from functions import *
 
 
-
-## Tested functions ##
-
+### Required functions ###
 
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     """
@@ -62,11 +61,11 @@ def ridge_regression(y, tx, lambda_):
     w = np.linalg.solve(a, b)
     return (w, compute_loss(y, tx, w))
 
-def logistic_regression(y, tx, initial_w, max_iters, gamma,losses_return = False):
+def logistic_regression(y, tx, initial_w, max_iters, gamma, losses_return = False):
     """
     Perform logistic regression using gradient descent.
     Parameters:
-    y (numpy.ndarray): The target variable.
+    y (numpy.ndarray): The target variable. Binary (0,1)
     tx (numpy.ndarray): The input features.
     initial_w (numpy.ndarray): The initial weight vector.
     max_iters (int): The maximum number of iterations.
@@ -88,7 +87,7 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma,losses_return = False
         return w, loss, losses
     return w, loss,
 
-def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma,return_losses=False):
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma, return_losses=False):
     """
     Regularized logistic regression using gradient descent.
     Parameters:
@@ -118,268 +117,3 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma,return_l
     return w, loss
 
 
-
-## Helper functions ##
-
-
-def test_function(func):
-    y = np.array([5, 10, 17])
-    tx = np.array([[1, 2], [1, 3], [1, 4]])
-    initial_w = np.array([0, 0])
-    max_iters = 1000
-    gamma = 0.1
-    a = func(y, tx, initial_w, max_iters, gamma)
-    print(a[0], "MSE : ", a[1])
-
-def compute_loss(y, tx, w):
-    """Calculate the loss using MSE.
-
-    Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        w: numpy array of shape=(2,). The vector of model parameters.
-
-    Returns:
-        the value of the loss (a scalar), corresponding to the input parameters w.
-    """
-    N = np.shape(y)[0]
-    e = y - np.dot(tx, w)
-    return 1 / (2 * N) * (e.T.dot(e))
-
-
-
-### Gradient Descent ###
-
-
-def compute_gradient(y, tx, w):
-    """Computes the gradient at w.
-
-    Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        w: numpy array of shape=(2, ). The vector of model parameters.
-
-    Returns:
-        An numpy array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
-    """
-
-    N = np.shape(y)[0]
-    e = y - np.dot(tx, w)
-    grad = -1 / N * np.dot(tx.T, e)
-    return grad
-
-def gradient_descent(y, tx, initial_w, max_iters, gamma):
-    """The Gradient Descent (GD) algorithm.
-
-    Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
-        max_iters: a scalar denoting the total number of iterations of GD
-        gamma: a scalar denoting the stepsize
-
-    Returns:
-        losses: a list of length max_iters containing the loss value (scalar) for each iteration of GD
-        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of GD
-    """
-    # Define parameters to store w and loss
-    ws = [initial_w]
-    w = initial_w
-    loss = compute_loss(y, tx, w)
-    losses = [loss]
-    for n_iter in range(max_iters):
-        gradient = compute_gradient(y, tx, w)
-        w = w - gamma * (gradient)
-        # store w and loss
-        ws.append(w)
-        loss = compute_loss(y, tx, w)
-        losses.append(loss)
-        # print(
-        #     "GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
-        #         bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
-        #     )
-        # )
-
-    return ws, losses
-
-
-
-
-### Stochastic Gradient Descent ###
-
-
-def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma, batch_size=1):
-    """The Stochastic Gradient Descent algorithm (SGD).
-
-    Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
-        batch_size: a scalar denoting the number of data points in a mini-batch used for computing the stochastic gradient
-        max_iters: a scalar denoting the total number of iterations of SGD
-        gamma: a scalar denoting the stepsize
-
-    Returns:
-        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SGD
-        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SGD
-    """
-
-    # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
-    w = initial_w
-
-    for n_iter in range(max_iters):
-        gradient = compute_stoch_gradient(y, tx, w, batch_size)
-        w = w - gamma * gradient
-        # store w and loss
-        ws.append(w)
-        loss = compute_loss(y, tx, w)
-        losses.append(loss)
-        # print(
-        #     "SGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
-        #         bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
-        #     )
-        # )
-    return ws, losses
-
-def compute_stoch_gradient(y, tx, w, batch_size=1):
-    """Compute a stochastic gradient at w from a data sample batch of size B, where B < N, and their corresponding labels.
-
-    Args:
-        y: numpy array of shape=(B, )
-        tx: numpy array of shape=(B,2)
-        w: numpy array of shape=(2, ). The vector of model parameters.
-
-    Returns:
-        A numpy array of shape (2, ) (same shape as w), containing the stochastic gradient of the loss at w.
-    """
-    batches = batch_iter(y, tx, batch_size=batch_size, num_batches=1, shuffle=True)
-    gradient = np.mean(
-        [compute_gradient(y_batch, tx_batch, w) for y_batch, tx_batch in batches],
-        axis=0,
-    )
-    return gradient
-
-def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
-    """
-    Generate a minibatch iterator for a dataset.
-    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
-    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
-    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
-
-    Example:
-
-     Number of batches = 9
-
-     Batch size = 7                              Remainder = 3
-     v     v                                         v v
-    |-------|-------|-------|-------|-------|-------|---|
-        0       7       14      21      28      35   max batches = 6
-
-    If shuffle is False, the returned batches are the ones started from the indexes:
-    0, 7, 14, 21, 28, 35, 0, 7, 14
-
-    If shuffle is True, the returned batches start in:
-    7, 28, 14, 35, 14, 0, 21, 28, 7
-
-    To prevent the remainder datapoints from ever being taken into account, each of the shuffled indexes is added a random amount
-    8, 28, 16, 38, 14, 0, 22, 28, 9
-
-    This way batches might overlap, but the returned batches are slightly more representative.
-
-    Disclaimer: To keep this function simple, individual datapoints are not shuffled. For a more random result consider using a batch_size of 1.
-
-    Example of use :
-    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
-        <DO-SOMETHING>
-    """
-    data_size = len(y)  # NUmber of data points.
-    batch_size = min(data_size, batch_size)  # Limit the possible size of the batch.
-    max_batches = int(
-        data_size / batch_size
-    )  # The maximum amount of non-overlapping batches that can be extracted from the data.
-    remainder = (
-        data_size - max_batches * batch_size
-    )  # Points that would be excluded if no overlap is allowed.
-
-    if shuffle:
-        # Generate an array of indexes indicating the start of each batch
-        idxs = np.random.randint(max_batches, size=num_batches) * batch_size
-        if remainder != 0:
-            # Add an random offset to the start of each batch to eventually consider the remainder points
-            idxs += np.random.randint(remainder + 1, size=num_batches)
-    else:
-        # If no shuffle is done, the array of indexes is circular.
-        idxs = np.array([i % max_batches for i in range(num_batches)]) * batch_size
-
-    for start in idxs:
-        start_index = start  # The first data point of the batch
-        end_index = (
-            start_index + batch_size
-        )  # The first data point of the following batch
-        yield y[start_index:end_index], tx[start_index:end_index]
-
-
-
-
-### Logistic Regression ###
-
-
-def sigmoid(t):
-    sigmoid = 1 / (1 + np.exp(-t))
-    sigmoid = np.where(sigmoid==0,1e-10,sigmoid)
-    sigmoid = np.where(sigmoid==1,1-1e-10,sigmoid)
-    return sigmoid
-
-def logistic_gradient(y, tx, w):
-    y_pred = sigmoid(tx @ w)
-    gradient = np.dot(tx.T, y_pred - y) / y.shape[0]
-    return gradient
-
-def penalized_logistic_regression(y, tx, w, lambda_):
-    N = y.shape[0]
-    gradient = logistic_gradient(y, tx, w) + lambda_ * 2 * w  # penalized gradient
-    return gradient
-
-def logistic_loss(y, tx, w):
-    N = tx.shape[0]
-    loss=(-1/ N* np.sum(y * np.log(sigmoid(tx @ w)) + (1 - y) * np.log(1 - sigmoid(tx @ w))))
-    return loss
-
-
-
-
-
-
-
-### Evaluation ###
-
-
-def f1_score(y_true, y_pred):
-    """
-    Compute the F1 score.
-    Parameters:
-    y_true (numpy.ndarray): The true labels.
-    y_pred (numpy.ndarray): The predicted labels.
-    Returns:
-    float: The F1 score.
-    """
-    tp = np.sum(y_true * y_pred)
-    fp = np.sum((1 - y_true) * y_pred)
-    fn = np.sum(y_true * (1 - y_pred))
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
-    return 2 * (precision * recall) / (precision + recall)
-
-
-def accuracy(y_true, y_pred):
-    """
-    Compute the accuracy.
-    Parameters:
-    y_true (numpy.ndarray): The true labels.
-    y_pred (numpy.ndarray): The predicted labels.
-    Returns:
-    float: The accuracy.
-    """
-    return np.mean(y_true == y_pred)
--- preprocessing.py
+++ preprocessing.py
@@ -25,81 +25,6 @@ def correlation(data, threshold=0.95):
     return list(to_remove)
 
 
-def columns_to_drop(data, correlation_thr=0.95, nan_thr=0.9, std_thr=0.1):
-    """
-    This function returns the columns to drop in the data, namely:
-    - columns with high number of missing values
-    - columns with low std
-    - correlated columns
-    
-    parameters:
-    data: the data
-    correlation_thr: the columns with correlation score above this threshold will be dropped
-    nan_thr: the columns with more than this threshold ratio of NaN values will be dropped
-    std_thr: the columns with std below this threshold will be dropped
-    
-    returns: list of columns to drop
-    
-    """
-    columns_to_drop = []
-
-    # drop columns with more than 90% missing values
-    nan_col = np.isnan(data).sum(axis=0)
-    columns_missing_values = np.where(nan_col > (nan_thr * data.shape[0]))[0].tolist()
-    columns_to_drop.extend(columns_missing_values)
-    print(
-        f"Number of columns with more than {nan_thr} NaN:", len(columns_missing_values)
-    )
-
-    # drop columns with std < 0.1
-    std_devs = np.nanstd(data, axis=0)  # std, ignoring NaNs
-    low_std_mask = std_devs < std_thr  # mask of columns with std < 0.1
-    columns_constant = np.where(low_std_mask)[0].tolist()
-    columns_to_drop.extend(columns_constant)
-    print(f"Number of columns with std < {std_thr}:", len(columns_constant))
-
-    # drop perfectly correlated columns
-    columns_correlated = correlation(data, correlation_thr)
-    columns_to_drop.extend(columns_correlated)
-    print(f"Number of perfectly correlated columns:", len(columns_correlated))
-
-    return list(set(columns_to_drop))  # removes duplicates
-
-
-def clean_data(x_train, x_test, correlation_thr=0.95, nan_thr=0.9, std_thr=0.1):
-    """
-    This function cleans the data by dropping columns and handling missing values.
-    
-    returns: cleaned train and test data
-    """
-    to_drop = columns_to_drop(x_train, correlation_thr, nan_thr, std_thr)
-    print("Columns to drop:", to_drop)
-    clean_train = np.delete(x_train, to_drop, axis=1)
-    clean_test = np.delete(x_test, to_drop, axis=1)
-
-    # Handle missing values
-    clean_train, clean_test = handle_nan(clean_train), handle_nan(clean_test)
-
-    # Standardize the data
-    standardized_train = (clean_train - np.mean(clean_train, axis=0)) / np.std(
-        clean_train, axis=0
-    )
-    standardized_test = (clean_test - np.mean(clean_test, axis=0)) / np.std(
-        clean_test, axis=0
-    )
-
-    # Check for correlation post processing
-    columns_correlated = correlation(standardized_train)
-    print(
-        "Number of perfectly correlated columns after cleaning:",
-        len(columns_correlated),
-    )
-    final_train = np.delete(standardized_train, columns_correlated, axis=1)
-    final_test = np.delete(standardized_test, columns_correlated, axis=1)
-
-    return final_train, final_test
-
-
 def standardize(data):
     """
     This function standardizes the data.
@@ -129,7 +54,7 @@ def compute_mode(column):
     return unique[np.argmax(counts)]  # return the value with the highest count
 
 
-def handle_nan(data):
+def replace_nan(data):
     """
     This function replaces NaN values with the mean of the column for non-binary columns and the mode for binary columns.
 
@@ -162,6 +87,7 @@ def split_data(x, y, ratio, seed=1):
 def threshold(y_pred):
     mean_data = np.mean(y_pred)
     return np.where(y_pred >= mean_data, -1, 1)
+
 
 def PCA(xtrain,num_axis,graph=False):
     """
@@ -215,25 +141,28 @@ def PCA(xtrain,num_axis,graph=False):
     
     return xtrain_pca
 
-
     
-def i_j_rule_treshold(data,treshold):
+def nan_score_filter(data, treshold):
     """
     This function returns once the columns/rows having their median above a treshold .
     """
     col_med=np.median(data,axis=0)
     row_med=np.median(data,axis=1)
-    print("Max median NaN score rows : ",np.max(row_med))
-    print("Max median NaN score columns : ",np.max(col_med))
-    i_drop=np.where(row_med>treshold)
-    j_drop=np.where(col_med>treshold)
-    return i_drop[0],j_drop[0]
+    
+    print("Max median NaN score rows : ", np.max(row_med))
+    print("Max median NaN score columns : ", np.max(col_med))
+    
+    rows_to_drop=np.where(row_med>treshold)
+    cols_to_drop=np.where(col_med>treshold)
+    
+    return rows_to_drop[0],cols_to_drop[0]
 
-def alternative_nan_handeling(data_x,data_y,data_x_to_predict=None,nan_trd=.5):
+
+def drop_nan(x_train, y_train, x_test=None, nan_thr=.5):
     """
     This function drops the rows and columns with NaN values above a certain threshold.
     The criterion is computed, for each item of the array by taking the square root of the product of the NaN ratio of the row and column.
-    Criterion: NaN_score(i,j)=sqrt(NaN_ratio_row(i)*NaN_ratio_col(j))
+    Criterion: NaN_score(i,j) = sqrt(NaN_ratio_row(i) * NaN_ratio_col(j))
     All the rows and columns having a single computed NaN score above the threshold are dropped.
     
     arguments:
@@ -243,38 +172,50 @@ def alternative_nan_handeling(data_x,data_y,data_x_to_predict=None,nan_trd=.5):
     nan_trd: the threshold for the NaN ratio
     
     returns: x_data with reduced rows and columns, y_data with reduced rows
-    
     """
-    assert (data_x_to_predict is None) or (data_x_to_predict.shape[1]==data_x.shape[1]), "The number of columns in the data to predict should be the same as the input data"
-    if data_x_to_predict is None:
-        data_x_to_predict=np.array([])
-    cum_x=np.concatenate((data_x,data_x_to_predict),axis=0)
-    nan_col_ratio=(np.isnan(cum_x).sum(axis=0)/data_x.shape[0])
-    nan_row_ratio=(np.isnan(cum_x).sum(axis=1)/data_x.shape[1])
-    x,y=np.meshgrid(nan_col_ratio,nan_row_ratio)
-    # to find a score for each row and column that take into account the NaN ratio
-    # we can multiply the two matrices and take the square root
-    dual_ratio=np.sqrt(x*y)
-    i_drop,j_drop=i_j_rule_treshold(dual_ratio,nan_trd)
-    #the rows to drop should be only in the data_x avoiding the data_x_to_predict
-    i_drop_=i_drop[i_drop<data_x.shape[0]]
-    #return the data with the rows and columns dropped
-    w_row_x=np.delete(data_x,i_drop_,axis=0)
-    w_colrow_x=np.delete(w_row_x,j_drop,axis=1)
-    # reduce the y data accordingly to the rows dropped
-    w_row_y=np.delete(data_y,i_drop_,axis=0)
     
-    print(f"Number of rows dropped because of a NaN score > {nan_trd}: ",i_drop_.shape[0])
-    print(f"Number of columns dropped because of a NaN score > {nan_trd}: ",j_drop.shape[0])
+    # check if the number of columns in the test data is the same as the train data
+    assert (x_test is None) or (x_test.shape[1] == x_train.shape[1])
     
-    if data_x_to_predict.size!=0:
-        # reduce the x_data_to_predict only on the columns
-        w_col_x_to_predict=np.delete(data_x_to_predict,j_drop,axis=1)
-        return w_colrow_x , w_row_y , w_col_x_to_predict
+    if x_test is None:
+        x_test=np.array([])
+        
+        
+    cum_x = np.concatenate((x_train, x_test),axis=0) 
     
-    return w_colrow_x , w_row_y
+    # compute the NaN ratio for each row and column
+    nan_col_ratio = np.isnan(cum_x).sum(axis=0) / x_train.shape[0]
+    nan_row_ratio = np.isnan(cum_x).sum(axis=1) / x_train.shape[1]
+    
+    # create a meshgrid of the two arrays to compute the NaN score
+    x, y = np.meshgrid(nan_col_ratio, nan_row_ratio) 
+    
+    # compute the NaN score
+    nan_score = np.sqrt(x * y) # the NaN score for each row and column
+    
+    # identify rows and columns to drop based on NaN score threshold
+    rows_to_drop, cols_to_drop = nan_score_filter(nan_score, nan_thr) 
+    
+    # the rows to drop should be only in the train data avoiding (not test data)
+    rows_to_drop = rows_to_drop[rows_to_drop < x_train.shape[0]]
+    
+    # apply the drops on train data
+    x_train_clean = np.delete(x_train,rows_to_drop, axis=0)
+    x_train_clean = np.delete(x_train_clean,cols_to_drop, axis=1)
+    y_train_clean = np.delete(y_train,rows_to_drop, axis=0)
+    
+    print(f"Number of rows dropped because of a NaN score > {nan_thr}: ", rows_to_drop.shape[0])
+    print(f"Number of columns dropped because of a NaN score > {nan_thr}: ", cols_to_drop.shape[0])
+    
+    if x_test.size != 0:
+        # reduce the test data only on the columns
+        x_test_clean = np.delete(x_test, cols_to_drop, axis=1)
+        return x_train_clean , y_train_clean , x_test_clean
+    
+    return x_train_clean , y_train_clean
 
-def Edited_clean_data(x_train,y_train, x_test, correlation_thr=0.95, nan_thr=0.5, std_thr=0.1):
+
+def clean_data(x_train_, y_train_, x_test_, correlation_thr=0.95, nan_thr=0.5, std_thr=0.1):
     """
     This function cleans the data by dropping columns and handling missing values.
     
@@ -282,54 +223,48 @@ def Edited_clean_data(x_train,y_train, x_test, correlation_thr=0.95, nan_thr=0.5
     """
     
     # Drop rows and columns with NaN scores above a certain threshold
-    x , final_y , x_toPred = alternative_nan_handeling(x_train,y_train,x_test,nan_thr)
+    x_train , y_train , x_test = drop_nan(x_train_, y_train_, x_test_, nan_thr)
+        
+    X = np.concatenate((x_train, x_test), axis=0)
     
-    #we now only influence the columns of x and x_toPred
-    
-    cumX=np.concatenate((x,x_toPred),axis=0)
-    
-    # drop columns with std < std_thr
-    std_devs = np.nanstd(cumX, axis=0)  # std, ignoring NaNs
-    low_std_mask = std_devs < std_thr  # mask of columns with std < 0.1
-    columns_constant = np.unique(np.where(low_std_mask)[0])
+    # Drop columns with std < std_thr
+    std_devs = np.nanstd(X, axis=0)                         # std, ignoring NaNs
+    low_std_mask = std_devs < std_thr                       # mask of columns with std < 0.1
+    columns_constant = np.unique(np.where(low_std_mask)[0]) # columns with std < 0.1, we keep only unique values
     print(f"Number of columns with std < {std_thr}:", columns_constant.shape[0])
-    #we delete all the columns with std below the threshold
-    x_wstd=np.delete(cumX,columns_constant,axis=1)
+    x_wstd = np.delete(X, columns_constant, axis=1)    
 
-    # drop columns with correlation score above correlation_thr
+    # Drop columns with correlation score above correlation_thr
     columns_correlated = correlation(x_wstd, correlation_thr)
-    
     print(f"Number of columns with correl_coef > {correlation_thr}:", len(columns_correlated))
-    
     x_wstd_wcor = np.delete(x_wstd, columns_correlated, axis=1)
 
     # Handle missing values
-    clean_X= handle_nan(x_wstd_wcor)
+    clean_X = replace_nan(x_wstd_wcor)
 
     # Standardize the data
-    stand_X=standardize(clean_X)
-    
-    
+    stand_X = standardize(clean_X)
     
     # Check for correlation post processing
-    columns_correlated_f = correlation(stand_X,correlation_thr)
-    print(f"Number of columns with corr_coef> {correlation_thr} after cleaning:",len(columns_correlated_f))
-    
+    columns_correlated_f = correlation(stand_X, correlation_thr)
+    print(f"Number of columns with corr_coef> {correlation_thr} after cleaning:", len(columns_correlated_f))
     final_X = np.delete(stand_X, columns_correlated_f, axis=1)
     
-    #return the cleaned data by deconcatenating the x and x_toPred
-    fi_x=final_X[:x.shape[0],:]
-    fi_x_toPred=final_X[x.shape[0]:,:]
+    # Return the cleaned data by deconcatenating the x_train and x_test
+    cleaned_x_train = final_X[:x_train.shape[0],:]
+    cleaned_x_test = final_X[x_train.shape[0]:,:]
     
-    assert np.isnan(final_X).sum()==0, "The data should not contain NaN values"
-    assert fi_x.shape[1]==fi_x_toPred.shape[1], "The number of columns in the cleaned data should be the same for the data to predict"
-    assert fi_x.shape[0]==final_y.shape[0], "The number of rows in the cleaned data should be the same as the output data"
+    assert np.isnan(final_X).sum() == 0                          # The data should not contain NaN values
+    assert cleaned_x_train.shape[1] == cleaned_x_test.shape[1]   # The number of columns in the train data should be the same for the test data 
+    assert cleaned_x_train.shape[0] == y_train.shape[0]          # The number of rows in the cleaned data should be the same as the output data
     
     
     print("The data has been cleaned and standardized")
-    print("The cleaned x-data has the following shape: ",fi_x.shape)
-    print("The cleaned y-data has the following shape: ",final_y.shape)
-    print("The cleaned x-data-to-predict has the following shape: ",fi_x_toPred.shape)
+    print("The cleaned x-data has the following shape: ", cleaned_x_train.shape)
+    print("The cleaned y-data has the following shape: ", y_train.shape)
+    print("The cleaned x-data-to-predict has the following shape: ", cleaned_x_test.shape)
     
     
-    return fi_x,fi_x_toPred,final_y
+    return cleaned_x_train, cleaned_x_test, y_train
+
+
